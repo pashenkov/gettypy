cc77eed1c11930d6bae37ffe2740e9d4b3012a19
gucheng0712 <gucheng0712@126.com>
2019-04-22 16:48:58 -0700

    Update emotion-age-gender-tcp.py

--- emotion-age-gender-tcp.py
+++ emotion-age-gender-tcp.py
@@ -156,6 +156,26 @@ def emotionModel():
 	emotion_model.load_weights('../models/facial_expression_model_weights.h5') #load weights
 	return emotion_model
 
+# return the most frequency
+def mostFrequent(arr, n):
+    arr.sort()
+    max_count = 1;
+    res = arr[0];
+    curr_count = 1
+    
+    for i in range(1, n):
+        if (arr[i] == arr[i - 1]):
+            curr_count += 1
+        else:
+            if (curr_count > max_count):
+                max_count = curr_count
+                res = arr[i - 1]
+            curr_count = 1
+    if (curr_count > max_count):
+        max_count = curr_count
+        res = arr[n - 1]
+    return res
+
 age_model = ageModel()
 gender_model = genderModel()
 emotion_model = emotionModel()
