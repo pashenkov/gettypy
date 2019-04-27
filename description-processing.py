d457ec48fdac6e54a58dddcd0bc6384b16b5f6c4
Cheng Gu <gucheng0712@126.com>
2019-04-26 20:11:50 -0700

    actually no need check if sentence is none

--- description-processing.py
+++ description-processing.py
@@ -87,24 +87,20 @@ for response in responses:
         if not (hasNoun and (hasVerbD or hasVerbG)):
             print("- removing Sentences doesn't have noun or verb:")
             print(sentence)
-            if(sentence in response_sentences):
-                response_sentences.remove(sentence)
-        if (len(sentence) < MIN_LENGTH_SENTENCE):
+            response_sentences.remove(sentence)
+        elif (len(sentence) < MIN_LENGTH_SENTENCE):
             print("- removing too short sentence:")
             print(sentence)
-            if(sentence in response_sentences):
-                response_sentences.remove(sentence)
-        if (sentence[len(sentence) - 1] != '.'):
+            response_sentences.remove(sentence)
+        elif (sentence[len(sentence) - 1] != '.'):
             # sentence does not end with '.'
             print("- removing incomplete sentence:")
             print(sentence)
-            if(sentence in response_sentences):
-                response_sentences.remove(sentence)
-        if (not sentence[0].isupper()):  # if not starting as capital Character which means the word is broken
+            response_sentences.remove(sentence)
+        elif (not sentence[0].isupper()):  # if not starting as capital Character which means the word is broken
             print("- removing the sentence which the first word maybe broken")
             print(sentence)
-            if(sentence in response_sentences):
-                response_sentences.remove(sentence)
+            response_sentences.remove(sentence)
 
     for i in range(len(response_sentences)):
         sentence = response_sentences[i]
